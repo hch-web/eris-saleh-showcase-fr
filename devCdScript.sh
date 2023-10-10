20cbbcf78e9cd1a570ec939f03d6d2db406ecba5
Ubuntu <ubuntu@ip-172-31-11-117.me-south-1.compute.internal>
2023-10-10 08:20:37 +0000

    nvm added in devCdscript

--- devCdScript.sh
+++ devCdScript.sh
@@ -11,5 +11,13 @@ else
   exit 1;
 fi
 
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+nvm install v18.18.0
+nvm use v18.18.0
+
 npm run build;
+
 sudo service nginx restart
